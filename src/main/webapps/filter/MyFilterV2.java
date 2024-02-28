286933501de90d4c4dbf6e86d4b79f6c0fbb8864
JungMYEONG-jin <dwrhksfl77@naver.com>
2024-02-28 13:09:28 +0900

    전역 필터에 request encoding 설정.

--- src/main/webapps/filter/MyFilterV2.java
+++ src/main/webapps/filter/MyFilterV2.java
@@ -2,16 +2,21 @@ package main.webapps.filter;
 
 import javax.servlet.*;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class MyFilterV2 implements Filter {
+
+    String code;
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         System.out.println("MyFilterV2 init");
+        code = filterConfig.getInitParameter("charset");
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         System.out.println("====MyFilterV2 in====");
+        servletRequest.setCharacterEncoding(code);
         filterChain.doFilter(servletRequest, servletResponse);
         System.out.println("====MyFilterV2 out====");
     }
